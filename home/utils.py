bc3efdcada812eebe09f34e85bd0503293e0fa54
OQdevpy <o.qosimov@tuit.uz>
2023-09-20 01:16:33 +0500

    push

--- home/utils.py
+++ home/utils.py
@@ -1,23 +1,26 @@
 import requests
-
+from .models import Token
 base_url = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
 
 
-headers = {
-    "X-RapidAPI-Key": "fc925ae769msh9314f40d1cfad06p11c327jsn2d390b9482b4",
-    "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"
-}
 
 
 def play_count(url):
-    querystring = {"url": url, "hd": "1"}
-    response = requests.get(base_url, headers=headers, params=querystring)
-    data = response.json()['data']
-    try:
-        return [data['play_count'],
-                data['digg_count'], 
-                data['collect_count'], 
-                data['share_count'], 
-                data['comment_count']]
-    except Exception:
-        return 0
+    tokens = Token.objects.all()
+    headers = {
+        "X-RapidAPI-Key": "fc925ae769msh9314f40d1cfad06p11c327jsn2d390b9482b4",
+        "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"
+    }
+    for i in tokens:
+        headers['X-RapidAPI-Key'] = i.RapidApiKey
+        querystring = {"url": url, "hd": "1"}
+        response = requests.get(base_url, headers=headers, params=querystring)
+        data = response.json()['data']
+        try:
+            return [data['play_count'],
+                    data['digg_count'], 
+                    data['collect_count'], 
+                    data['share_count'], 
+                    data['comment_count']]
+        except Exception as e:
+            continue
